6dddc58ee5c1e4123c9ba9543528428b332a986e
Honei12 <hannes.schuerer.student@htl-hallein.at>
2019-07-09 10:07:50 +0200

    pictures

--- TakePicturesLocals/takePicturesLocal.py
+++ TakePicturesLocals/takePicturesLocal.py
@@ -15,7 +15,8 @@ while(True):
     cv2.imshow('frame',frame)
 
     if cv2.waitKey(1) == 32:
-        cv2.imwrite("image_"+str(counter)+".png", frame);
+        print("Image taken")
+        cv2.imwrite("image_"+str(counter)+".jpg", frame);
         counter = counter + 1;
 
     if cv2.waitKey(1) == 27:
