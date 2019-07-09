f58b25e05ed53dab63bee46968c4804e30b9b6d1
unknown <rettenbacher.phil@gmail.com>
2019-07-09 09:17:37 +0200

    TakePicturesLocal

---
+++ TakePicturesLocals/takePicturesLocal.py
@@ -0,0 +1,26 @@
+import cv2;
+
+#The onboard webcam usually has the id 0
+webcamID = 1;
+
+cap = cv2.VideoCapture(webcamID);
+
+counter = 0;
+
+while(True):
+    # Capture frame-by-frame
+    ret, frame = cap.read()
+
+    # Display the resulting frame
+    cv2.imshow('frame',frame)
+
+    if cv2.waitKey(1) == 32:
+        cv2.imwrite("image_"+str(counter)+".png", frame);
+        counter = counter + 1;
+
+    if cv2.waitKey(1) == 27:
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
