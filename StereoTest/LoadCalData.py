40a33a8417ea6b41743217bc3cd2e3bfed2e5b53
Honei12 <hannes.schuerer.student@htl-hallein.at>
2019-07-10 16:08:05 +0200

    Data

--- StereoTest/LoadCalData.py
+++ StereoTest/LoadCalData.py
@@ -6,11 +6,16 @@ import cv2
 cam = ImagingApi.CameraApi(False, (1,2))
 
 calib_loaded = calibration.StereoCalibration(input_folder=os.getcwd()+"/CalData")
+
 mindisparity = 2
-numdisparites = 64
+numdisparites = 80
 blocksize = 15
-specklerange = 12
-specklewindowsize = 2
+specklerange = 1
+specklewindowsize = 12
+preFilterSize = 0
+smallerBlockSize = 10
+textureThreshold = 1
+
 while True:
 
     left_image, right_image = cam.getPicture()
@@ -23,7 +28,7 @@ while True:
     cv2.imshow("1",rectified_pair[0])
     cv2.imshow("2",rectified_pair[1])
 
-    block_matcher = cv2.StereoBM_create(numDisparities=16, blockSize=15)
+    block_matcher = cv2.StereoSGBM_create()
 
 
     block_matcher.setMinDisparity(mindisparity)
@@ -31,15 +36,18 @@ while True:
     block_matcher.setBlockSize(blocksize)
     block_matcher.setSpeckleRange(specklerange)
     block_matcher.setSpeckleWindowSize(specklewindowsize)
+    block_matcher.setPreFilterCap(preFilterSize)
+    #block_matcher.setSmallerBlockSize(smallerBlockSize)
+    #block_matcher.setTextureThreshold(textureThreshold)
 
     disparity = block_matcher.compute(rectified_pair[0],rectified_pair[1])
     cv2.imshow('Ja', disparity / 1024.)
     cv2.waitKey(1)
 
     if cv2.waitKey(10) == 32:
-        blocksize+=10
+        preFilterSize+=10
 
-    print(blocksize)
+    print(preFilterSize)
 
 #disparity = block_matcher.compute(rectified_pair[0], rectified_pair[1])
 
