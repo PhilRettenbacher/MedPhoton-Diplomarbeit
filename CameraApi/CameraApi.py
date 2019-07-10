85eec87ff134033e25ccc6882064b35df04cb9b0
Phil <philipp.rettenbacher.student@htl-hallein.at>
2019-07-10 08:38:43 +0200

    CameraAPI init

--- CameraApi/CameraApi.py
+++ CameraApi/CameraApi.py
@@ -1,3 +1,12 @@
-class CameraApi:
+import cv2;
 
-    i = 123
+class CameraApi:
+    def __init__(self, useWeb, id):
+
+        if(useWeb):
+            print("Initialize Camera in Web");
+        else:
+            print("Initialize Camera locally");
+            self.cap1 = cv2.VideoCapture(id[0], cv2.CAP_DSHOW);
+            self.cap2 = cv2.VideoCapture(id[1], cv2.CAP_DSHOW);
+
