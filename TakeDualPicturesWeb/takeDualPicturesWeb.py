825f2a230273d44262aeebd43804ef8d8430c08f
Tobias Zwinger <tobias.zwinger.student@htl-hallein.at>
2019-07-11 14:49:00 +0200

    TestPics

--- TakeDualPicturesWeb/takeDualPicturesWeb.py
+++ TakeDualPicturesWeb/takeDualPicturesWeb.py
@@ -3,10 +3,10 @@ from ImagingApi import ImagingApi
 import numpy as np;
 
 #The onboard webcam usually has the id 0
-webcamID = 2;
-webcamID2 = 1;
+webcamID = 1;
+webcamID2 = 2;
 
-cam = ImagingApi.CameraApi(True, (1,2))
+cam = ImagingApi.CameraApi(False, (2,0))
 
 cap = cv2.VideoCapture('http://192.168.199.3:808' + str(webcamID) + '/')
 cap2 = cv2.VideoCapture('http://192.168.199.3:808' + str(webcamID2) + '/')
@@ -35,9 +35,6 @@ while(True):
     cv2.imshow('frame',frame)
     cv2.imshow('frame2', frame2)
 
-    lv_x = frame.winfo_rootx()
-    lv_y = frame.winfo_rooty()
-    print (lv_x, lv_y)
 
     if cv2.waitKey(1) == 32:
         #Right Cam
