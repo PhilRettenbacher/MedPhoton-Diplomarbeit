51fea9f1fc1b747d75e6cd791f697c2755853e47
Phil <philipp.rettenbacher.student@htl-hallein.at>
2019-07-11 08:02:43 +0200

    BMTumer

--- StereoTest/LoadCalData.py
+++ StereoTest/LoadCalData.py
@@ -1,5 +1,6 @@
 from stereovision import calibration
 from ImagingApi import ImagingApi
+from stereovision.ui_utils import BMTuner
 import os
 import cv2
 
@@ -7,22 +8,25 @@ cam = ImagingApi.CameraApi(False, (1,2))
 
 calib_loaded = calibration.StereoCalibration(input_folder=os.getcwd()+"/CalData")
 
-while True:
-    left_image, right_image = cam.getPicture()
 
-    left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY)
-    right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY)
 
-    rectified_pair = calib_loaded.rectify((left_image, right_image))
+left_image, right_image = cam.getPicture()
 
-    cv2.imshow("1",rectified_pair[0])
-    cv2.imshow("2",rectified_pair[1])
+left_image = cv2.cvtColor(left_image, cv2.COLOR_BGR2GRAY)
+right_image = cv2.cvtColor(right_image, cv2.COLOR_BGR2GRAY)
 
-    block_matcher = cv2.StereoSGBM_create()
+rectified_pair = calib_loaded.rectify((left_image, right_image))
 
-    disparity = block_matcher.compute(rectified_pair[0],rectified_pair[1])
-    cv2.imshow('Ja', disparity / 1024.)
-    cv2.waitKey(1)
+cv2.imshow("1",rectified_pair[0])
+cv2.imshow("2",rectified_pair[1])
+
+block_matcher = cv2.StereoSGBM_create()
+
+tuner = BMTuner(block_matcher, calib_loaded, rectified_pair)
+
+disparity = block_matcher.compute(rectified_pair[0],rectified_pair[1])
+cv2.imshow('Ja', disparity / 1024.)
+cv2.waitKey(1)
 
 
 #disparity = block_matcher.compute(rectified_pair[0], rectified_pair[1])
