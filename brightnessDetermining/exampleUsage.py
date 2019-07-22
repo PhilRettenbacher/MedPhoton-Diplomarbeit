5a2c01ec43ba04baf3f18a753f4bf0215fc32cc2
Tobias Zwinger <tobias.zwinger.student@htl-hallein.at>
2019-07-22 10:10:19 +0200

    merge

--- brightnessDetermining/exampleUsage.py
+++ brightnessDetermining/exampleUsage.py
@@ -4,17 +4,19 @@ import cv2
 
 cap = cv2.VideoCapture(1, cv2.CAP_DSHOW)
 avergArr = bdr.setup()
+time = []
+
 counter = 0
 while True:
     a = datetime.datetime.now()
     ret, image = cap.read()
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    bdr.trueLoop(avergArr, image, False, counter)
+    bdr.trueLoop(avergArr, image, True, counter)
     counter += 1
 
     cv2.imshow("image", image)
     if cv2.waitKey(1) == 27:
         break
     b = datetime.datetime.now()
-    print("time: " + str(b - a))
+    print("Berechnungszeit: " + str(b-a))
