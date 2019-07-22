fe312c9810e21b4ea5438c9cb59e33b2be5417ad
Tobias Zwinger <tobias.zwinger.student@htl-hallein.at>
2019-07-22 09:23:48 +0200

    massiv improvement in speed in grafical representation

--- ImagingApi/Test.py
+++ ImagingApi/Test.py
@@ -5,6 +5,7 @@ import ImagingApi
 cap = ImagingApi.CameraApi(640,480,False, False) #resolution(with, height), put Pictures in separate folder, use web or local device search
 
 cap.keyListener()
+cap.makeVideo()
 
 
 
