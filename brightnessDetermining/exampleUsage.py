b7baec7529ec1a804fffd2bc980269b717f73a5d
Tobias Zwinger <tobias.zwinger.student@htl-hallein.at>
2019-07-23 07:57:34 +0200

    CoG added

--- brightnessDetermining/exampleUsage.py
+++ brightnessDetermining/exampleUsage.py
@@ -3,20 +3,24 @@ import datetime
 import cv2
 
 cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
-avergArr = bdr.setup()
+arrays = bdr.setup()
 time = []
 
 counter = 0
 while True:
     a = datetime.datetime.now()
     ret, image = cap.read()
-    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    try:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    except:
+        print('\033[1;31m Input error! No webcam found')
+        exit(0)
 
-    bdr.trueLoop(avergArr, image, True, counter, 1, 3, True)
+    bdr.trueLoop(arrays, image, True, counter, 10)
     counter += 1
 
     cv2.imshow("image", image)
     if cv2.waitKey(1) == 27:
         break
     b = datetime.datetime.now()
-    #print("Berechnungszeit: " + str(b-a))
+    print("Berechnungszeit: " + str(b-a))
