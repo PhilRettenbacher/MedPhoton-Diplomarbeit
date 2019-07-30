a6cdc6626a957c2c525069294e8564fcbb931b12
Phil <philipp.rettenbacher.student@htl-hallein.at>
2019-07-30 14:03:26 +0200

    new calibration things

--- Calibration/Test.py
+++ Calibration/Test.py
@@ -3,6 +3,7 @@ from Calibration import StereoCalibration
 from Calibration import MonoCalibration
 import numpy
 import datetime
+import matplotlib.pyplot as plt
 from brightnessDetermining import brightnessDiagramRealtime as bdr
 from ImagingApi import ImagingApi
 
@@ -20,14 +21,14 @@ calDataR = None
 #calculate the intrinsic and distortion parameters
 if(recalibrate):
 
-    lCount = 67
-    rCount = 60
+    lCount = 59
+    rCount = 38
 
     for x in range(0, lCount):
-        img = cv2.imread("CalibL/imageFrame_0_"+str(x)+".jpg")
+        img = cv2.imread("CalibR/imageFrame_0_"+str(x)+".jpg")
         calibL.addCheckerBoard(img)
     for x in range(0, rCount):
-        img = cv2.imread("CalibR/imageFrame_1_"+str(x)+".jpg")
+        img = cv2.imread("CalibR/imageFrame_0_"+str(x)+".jpg")
         calibR.addCheckerBoard(img)
 
     print("calibrating...")
@@ -49,24 +50,33 @@ else:
 
 calib = StereoCalibration.StereoCalibrator((8, 6), imLeft.shape[0:2], calDataL, calDataR)
 
+imLeft = cv2.imread("CalibR/imageFrame_0_0.jpg")
+imRight = cv2.imread("CalibRTest/imageFrame_0_0.jpg")
 
-dualCalCount = 34
+cv2.imshow("l", calib.undistort(imLeft, True))
+cv2.imshow("r", calib.undistort(imRight, False))
+cv2.waitKey(0)
+
+
+
+dualCalCount = 30
 dualCalStart = 0
 
 #calculate the rectification maps
 for x in range(dualCalStart, dualCalCount):
-    imLeft = cv2.imread("DualCalib/imageFrame_0_"+str(x)+".jpg")
-    imRight = cv2.imread("DualCalib/imageFrame_1_"+str(x)+".jpg")
+    imLeft = cv2.imread("DualCalibTest/imageFrame_0_"+str(x)+".jpg")
+    imRight = cv2.imread("DualCalibTest/imageFrame_1_"+str(x)+".jpg")
     ret = calib.addCheckerBoard(imLeft, imRight, False, False, 0)
     print(ret[0] and ret[1])
 
 
 calib.calibrate(shearing=True)
 
-imLeft = cv2.imread("DualCalib/imageFrame_0_0.jpg")
-imRight = cv2.imread("DualCalib/imageFrame_1_0.jpg")
+imLeft = cv2.imread("DualCalib/imageFrame_0_10.jpg")
+imRight = cv2.imread("DualCalib/imageFrame_1_10.jpg")
 iml = calib.rectifyImg(calib.undistort(imLeft, True), True)
 imr = calib.rectifyImg(calib.undistort(imRight, False), False)
+
 
 cv2.imshow("L", iml)
 cv2.imshow("R", imr)
@@ -76,10 +86,10 @@ cap = ImagingApi.CameraApi(640, 480)
 cap.keyListener()
 
 
-minDisp = -16*15
+minDisp = -16*10
 maxDisp = 16*5
 bm = cv2.StereoSGBM_create(minDisparity= minDisp, numDisparities=maxDisp-minDisp, blockSize=11, P2=3000, P1=1500, uniquenessRatio=0, speckleWindowSize=100, speckleRange=16, disp12MaxDiff=64, preFilterCap=3)
-arrays = bdr.setup()
+bright = bdr.brightnessDiagramRealtime()
 
 counter = 0
 sec = 0
@@ -95,11 +105,16 @@ while True:
     cv2.imshow("R", imr)
 
     disp = bm.compute(iml, imr)
+
     disp = (disp - (minDisp - 1) * 16) / (((maxDisp - minDisp)) * 16)
 
+    disp = disp.astype('float32')
+
+    disp = cv2.cvtColor(disp, cv2.COLOR_GRAY2RGB)
+    cv2.imshow("overlay", iml.astype("float32")*0.002+disp*0.7)
     # Graphics
 
-    bdr.trueLoop(arrays, sec, disp*1000, counter, 1, scaling=False, arr1=True, arr2=True, arr3=True, arr4=True)
+    #bright.trueLoop(disp*1000, counter, smoothed=True, scaling=False, arr1=True, arr2=True, arr3=True, arr4=True)
 
     counter += 1
 
